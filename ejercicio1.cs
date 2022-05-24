cd8b444a87332391711b62338825ee79d84559ca
emonge23 <mongeedgard@gmail.com>
2022-05-24 11:35:38 -0600

    Tercer commit, solicitud de segunda nota

--- ejercicio1.cs
+++ ejercicio1.cs
@@ -3,3 +3,6 @@ float n1, n2, n3;
 float promedio;
 Console.WriteLine("Digite nota 1 del estudiante");
 n1=float.parse(Console.ReadLine());
+Console.WriteLine("Digite nota 2 del estuidnate");
+n2=float.parse(Console.ReadLine());
+
